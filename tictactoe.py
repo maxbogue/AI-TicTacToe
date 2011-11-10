877711f946f9d5630950fcd786ff0754d0685ace
Max Bogue <max@maxbogue.com>
2011-11-10 00:09:09 -0500

    Convert player representation from True/False/None to 1/-1/0 in order to play nice with the MLP.

--- tictactoe.py
+++ tictactoe.py
@@ -2,9 +2,9 @@ n = 3
 
 def print_board(board):
     d = {
-        True: 'X',
-        False: 'O',
-        None: '-'
+        1: 'X',
+        -1: 'O',
+        0: '-'
     }
     for i in range(0, n*n, n):
         print("".join(d[board[j]] for j in range(i, i+n)))
@@ -13,27 +13,25 @@ def print_board(board):
 def next_states(board, p):
     ls = []
     for i in range(n*n):
-        if board[i] == None:
+        if board[i] == 0:
             b = list(board)
             b[i] = p
             ls.append(b)
     return ls
 
 def final_state(board, p):
-    # print_board(board)
-    if not any(map(lambda l: l == None, board)):
-        return 1
+    if not any(map(lambda x: x == 0, board)):
+        return 1 # Tie
     lines = [[], []]
-    r = range(n*n)
     for i in range(n):
         lines[0].append(i * (n + 1))
         lines[1].append((i + 1) * (n - 1))
-        lines.append(list(filter(lambda x: x % n == i, r)))
-        lines.append(list(filter(lambda x: x // n == i, r)))
+        lines.append(list(filter(lambda x: x % n == i, range(n*n))))
+        lines.append(list(filter(lambda x: x // n == i, range(n*n))))
     for line in lines:
         if all(map(lambda i: board[i] == p, line)):
             return 2
-        if all(map(lambda i: board[i] == (not p), line)):
+        if all(map(lambda i: board[i] == -p, line)):
             return 0
     return None
 
@@ -44,40 +42,40 @@ def minimax(board, p, a=None, b=None):
     else:
         v = 0
         for s in next_states(board, p):
-            v = max(v, 2 - minimax(s, not p, b, a))
+            v = max(v, 2 - minimax(s, -p, b, a))
             if b and v >= b:
                 return v
             a = max(a, v) if a else v
         return v
 
-# print(minimax([None for _ in range(n*n)], True))
+# print(minimax([0 for _ in range(n*n)], 1))
 assert minimax([
-    True, False, True,
-    False, None, None,
-    None, None, None
-], True) == 2
+    1, -1, 1,
+    -1, 0, 0,
+    0, 0, 0
+], 1) == 2
 assert minimax([
-    False, True, False,
-    True, False, True,
-    None, None, None
-], True) == 0
+    -1, 1, -1,
+    1, -1, 1,
+    0, 0, 0
+], 1) == 0
 assert minimax([
-    False, True, False,
-    True, None, True,
-    None, None, None
-], False) == 2
+    -1, 1, -1,
+    1, 0, 1,
+    0, 0, 0
+], -1) == 2
 assert minimax([
-    True, False, True,
-    False, True, None,
-    None, None, None
-], False) == 0
+    1, -1, 1,
+    -1, 1, 0,
+    0, 0, 0
+], -1) == 0
 assert minimax([
-    None, None, False,
-    None, True, None,
-    None, None, None
-], True) == 1
+    0, 0, -1,
+    0, 1, 0,
+    0, 0, 0
+], 1) == 1
 assert minimax([
-    None, None, None,
-    None, None, None,
-    None, None, None
-], True) == 1
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+], 1) == 1
