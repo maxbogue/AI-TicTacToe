445ca57bb81b1476cafa9a53b1b1ae5e01d95d6b
Max Bogue <max@maxbogue.com>
2011-11-09 21:43:08 -0500

    Add backprop learning!

--- mlp.py
+++ mlp.py
@@ -1,14 +1,24 @@
 from math import exp
 from random import random
 
-def sigmoid(x):
-    return 1 / (1 + exp(-x))
+mult = lambda x, y: x * y
 
-derivatives {
+sigmoid = lambda x: 1 / (1 + exp(-x))
+
+derivatives = {
     sigmoid: lambda x: sigmoid(x) * (1 - sigmoid(x)),
 }
+
 def zipWith(f, *ls):
     return map(lambda t: f(*t), zip(*ls))
+
+def dot(xs, ys):
+    # print(xs, ys)
+    assert len(xs) == len(ys)
+    return sum(zipWith(mult, xs, ys))
+
+def pprint(array):
+    print("[%s]" % "\n".join(map(str, array)))
 
 class MLP(object):
     """Represents a multi-layer perceptron.
@@ -58,14 +68,47 @@ class MLP(object):
         """
         v = [x] # raw values
         a = [x] # activated values
-        mult = lambda x, y: x * y
         for i, w in enumerate(self.weights):
-            raw = map(lambda r: sum(zipWith(mult, r, a[i])), self.weights)
-            v.append(list(raw))
-            a.append(list(map(self.act, raw)))
+            if i < len(self.weights) - 1:
+                a[i].insert(0, 1)
+            raw = list(map(lambda r: dot(r, a[i]), w))
+            activated = list(map(self.act, raw))
+            v.append(raw)
+            a.append(activated)
         if learning:
             return v, a
         else:
             return a[-1]
+    
+    def learn(self, t, v, a):
+        """Performs the backpropagation learning algorithm.
+        
+        t is the truth list.
+        v is the raw values for each node.
+        a is the activation value for each node.
+        
+        """
+        w = self.weights
+        da = derivatives[self.act] # da for derivative of activation
+        d = [[] for _ in range(len(w))] # d for deltas
+        
+        calc_bottom_delta = lambda i: (a[-1][i] - t[i]) * da(v[-1][i])
+        d[-1] = list(map(calc_bottom_delta, range(len(t))))
+        
+        for l in reversed(range(len(w))[:-1]):
+            delta = lambda i: dot([1] + d[l + 1], w[l][i]) * da(v[l+1][i])
+            d[l] = list(map(delta, range(len(w[l]))))
+        
+        for l in range(len(w)):
+            for j in range(len(w[l])):
+                for i in range(len(w[l][j])):
+                    w[l][j][i] = w[l][j][i] + self.lr * a[l][i] * d[l][j]
+        # pprint(d)
+    
+    def train(self, xs, ts):
+        """Train on """
+        for x, t in zip(xs, ts):
+            v, a = self.run(x, True)
+            self.learn(t, v, a)
     
 
